a62cc1de2a89db72320052f731896c0089c0d0d8
pmorgan <peterjmorgan@gmail.com>
2022-07-06 22:59:15 -0700

    added ListBranches() and ListFiles()

--- syringe_test.go
+++ syringe_test.go
@@ -67,3 +67,80 @@ func TestSyringe_ListProjects(t *testing.T) {
 		})
 	}
 }
+
+// 31479523
+func TestSyringe_ListFiles(t *testing.T) {
+	s, _ := NewSyringe("***REMOVED***")
+	type fields struct {
+		Gitlab *gitlab.Client
+	}
+	type args struct {
+		projectId int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []*gitlab.TreeNode
+		len     int
+		wantErr bool
+	}{
+		{"one", fields{s.Gitlab}, args{31479523}, nil, 5, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Syringe{
+				Gitlab: tt.fields.Gitlab,
+			}
+			got, err := s.ListFiles(tt.args.projectId, "main")
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ListFiles() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if reflect.TypeOf(got) != reflect.TypeOf(tt.want) {
+				t.Errorf("ListFiles() got = %v, want %v", got, tt.want)
+			}
+			if len(got) != tt.len {
+				t.Errorf("ListFiles() got = %v, want %v", len(got), tt.len)
+			}
+		})
+	}
+}
+
+func TestSyringe_ListBranches(t *testing.T) {
+	s, _ := NewSyringe("***REMOVED***")
+	type fields struct {
+		Gitlab *gitlab.Client
+	}
+	type args struct {
+		projectId int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []*gitlab.Branch
+		len     int
+		wantErr bool
+	}{
+		{"one", fields{s.Gitlab}, args{31479523}, nil, 20, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Syringe{
+				Gitlab: tt.fields.Gitlab,
+			}
+			got, err := s.ListBranches(tt.args.projectId)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ListBranches() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if reflect.TypeOf(got) != reflect.TypeOf(tt.want) {
+				t.Errorf("ListFiles() got = %v, want %v", got, tt.want)
+			}
+			if len(got) != tt.len {
+				t.Errorf("ListFiles() got = %v, want %v", len(got), tt.len)
+			}
+		})
+	}
+}
