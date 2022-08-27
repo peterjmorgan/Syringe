8199cbd6d10dbb6a4c46ced3ec99f9d96aa5a0f3
Peter Morgan <peterjmorgan@gmail.com>
2022-08-27 16:40:03 -0700

    ADO - ListFiles() working

--- internal/client/azure_test.go
+++ internal/client/azure_test.go
@@ -1,10 +1,12 @@
 package client
 
 import (
-	"github.com/peterjmorgan/Syringe/internal/structs"
-	"github.com/peterjmorgan/Syringe/internal/utils"
 	"reflect"
 	"testing"
+
+	"github.com/microsoft/azure-devops-go-api/azuredevops/git"
+	"github.com/peterjmorgan/Syringe/internal/structs"
+	"github.com/peterjmorgan/Syringe/internal/utils"
 )
 
 func TestNewAzureClient(t *testing.T) {
@@ -41,6 +43,43 @@ func TestAzureClient_ListProjects(t *testing.T) {
 
 			if len(*got) != tt.wantLen {
 				t.Errorf("Azure_ListProjects() len(projects) got = %v, want %v", len(*got), tt.wantLen)
+			}
+		})
+	}
+}
+
+func TestAzureClient_ListFiles(t *testing.T) {
+	setupEnv(t, "azure")
+	envMap, _ := utils.ReadEnvironment()
+	a := NewAzureClient(envMap, &testingSyringeOpts)
+
+	type args struct {
+		repoID string
+		branch string
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    []*git.GitItem
+		wantLen int
+		wantErr bool
+	}{
+		{"one", args{"4a7912a2-88a1-4697-9cf3-3919e9e07ddf", "master"}, []*git.GitItem{}, 5, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := a.ListFiles(tt.args.repoID, tt.args.branch)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ListFiles() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if reflect.TypeOf(got) != reflect.TypeOf(tt.want) {
+				t.Errorf("Azure_ListFiles() TypeOf got = %v, want %v", got, tt.want)
+			}
+
+			if len(got) != tt.wantLen {
+				t.Errorf("Azure_ListFiles() len(projects) got = %v, want %v", len(got), tt.wantLen)
 			}
 		})
 	}
