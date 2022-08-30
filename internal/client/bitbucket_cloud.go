ae304258957de630f37d6f270417de9e6367f4e7
Peter Morgan <peterjmorgan@gmail.com>
2022-08-29 17:55:18 -0700

    BitbucketCloud working!

--- internal/client/bitbucket_cloud.go
+++ internal/client/bitbucket_cloud.go
@@ -2,29 +2,38 @@ package client
 
 import (
 	"fmt"
+	"path/filepath"
+	"strings"
+	"sync"
+
 	"github.com/google/uuid"
 	"github.com/ktrysmt/go-bitbucket"
 	"github.com/peterjmorgan/Syringe/internal/structs"
+	"github.com/peterjmorgan/Syringe/internal/utils"
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/exp/slices"
 )
 
 type BitbucketCloudClient struct {
-	Client *bitbucket.Client
-	Owner  string
+	Client          *bitbucket.Client
+	Owner           string
+	ProjectMap      map[int64]*bitbucket.Repository
+	ProjectMapMutex sync.RWMutex
 }
 
-//TODO:
+// TODO:
 //  need: Owner
 func NewBitbucketCloudClient(envMap map[string]string, opts *structs.SyringeOptions) *BitbucketCloudClient {
-	//token := envMap["vcsToken"]
+	// token := envMap["vcsToken"]
 	owner := "peter_morgan_"
 
 	// Have to use Oauth2 client credentials config. Could not auth to the API any other way.
 	client := bitbucket.NewOAuthClientCredentials("APbFeKnRHr2zBk6v6w", "qP2aBzrzQzmDUbnHnYLScStwxDuHQTFV")
 
 	return &BitbucketCloudClient{
-		Client: client,
-		Owner:  owner,
+		Client:     client,
+		Owner:      owner,
+		ProjectMap: make(map[int64]*bitbucket.Repository, 0),
 	}
 }
 
@@ -56,6 +65,9 @@ func (b *BitbucketCloudClient) ListProjects() (*[]*structs.SyringeProject, error
 			Hydrated:  false,
 			GUID:      uuid,
 		})
+		temp := new(bitbucket.Repository)
+		*temp = item
+		b.ProjectMap[int64(uuid.ID())] = temp
 	}
 
 	return &retProjects, nil
@@ -88,4 +100,67 @@ func (b *BitbucketCloudClient) ListFiles(repoSlug string, branch string) (*[]*bi
 	}
 
 	return &retFiles, nil
+}
+
+func (b *BitbucketCloudClient) GetLockfilesByProject(projectId int64, mainBranchName string) ([]*structs.VcsFile, error) {
+	var retLockfiles []*structs.VcsFile
+
+	b.ProjectMapMutex.RLock()
+	repo := b.ProjectMap[projectId]
+	b.ProjectMapMutex.RUnlock()
+
+	projectFiles, err := b.ListFiles(repo.Name, repo.Mainbranch.Name)
+	if err != nil {
+		errStr := fmt.Sprintf("failed to GetLockfilesByProject for %v: %v\n", repo.Name, err)
+		log.Error(errStr)
+		return nil, fmt.Errorf(errStr)
+	}
+
+	supportedLockfiles := utils.GetSupportedLockfiles()
+
+	for _, file := range *projectFiles {
+		// var filePath string
+		fileName := filepath.Base(file.Path)
+		if slices.Contains(supportedLockfiles, fileName) || strings.HasSuffix(fileName, ".csproj") {
+			log.Debugf("Lockfile: %v in %v from project: %v\n", fileName, file.Path, repo.Name)
+			// download te file
+
+			// if !strings.Contains(file.Path, "/") {
+			// 	filePath = fmt.Sprintf("./%v", file.Path)
+			// } else {
+			// 	filePath = file.Path
+			// }
+
+			content, err := b.Client.Repositories.Repository.GetFileBlob(&bitbucket.RepositoryBlobOptions{
+				Owner:    b.Owner,
+				RepoSlug: repo.Name,
+				Ref:      mainBranchName,
+				Path:     file.Path,
+			})
+
+			// content, err := b.Client.Repositories.Repository.GetFileContent(&bitbucket.RepositoryFilesOptions{
+			// 	Owner:    b.Owner,
+			// 	RepoSlug: repo.Name,
+			// 	Ref:      mainBranchName,
+			// 	Path:     file.Path,
+			// 	MaxDepth: 500,
+			// })
+			if err != nil {
+				errStr := fmt.Sprintf("failed to GetFileContent for %v: %v\n", fileName, err)
+				log.Error(errStr)
+				return nil, fmt.Errorf(errStr)
+			}
+
+			retLockfiles = append(retLockfiles, &structs.VcsFile{
+				Name:          fileName,
+				Path:          file.Path,
+				Id:            file.Path,
+				Content:       content.Content,
+				PhylumProject: nil,
+			})
+
+		}
+	}
+
+	return retLockfiles, nil
 }
